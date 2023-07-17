ee65faafd2cef9026397e636fe8c8e6f6662a309
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-17 23:51:20 +0900

    [이미지 검색 앱] pixabay_api 생성

---
+++ lib/data/data_source/pixabay_api.dart
@@ -0,0 +1,10 @@
+class PixabayApi {
+  Future<PixabayResultDto> getImages(String query) async {
+    final url =
+        'https://pixabay.com/api/?key=37971457-4bad1cc05aa70353cb5a9d52e&q=$query&image_type=photo';
+    final http.Response response = await http.get(Uri.parse(url));
+
+    Map<String, dynamic> jsonString = jsonDecode(response.body);
+    return PixabayResultDto.fromJson(jsonString);
+  }
+}
